a9814d80c168bcc7d92b6cd95b6234c2df4f057d
rast <102649255+m3lody992@users.noreply.github.com>
2023-05-04 08:41:07 +0200

    rsietrste

--- Sources/Socal/UI/NbulujdD5678u9NhtdttpDhTSww34456UhdnnenTtl7678.swift
+++ Sources/Socal/UI/NbulujdD5678u9NhtdttpDhTSww34456UhdnnenTtl7678.swift
@@ -202,8 +202,11 @@ extension NbulujdD5678u9NhtdttpDhTSww34456UhdnnenTtl7678 {
 
     func WeHwft3DSzKISvkrSqqeg53QTFnS6FcG() {
         ATowJK3a6XSoyuEAXJhMV55Mo5x9iBfR { item in
-            guard let item = item else { return }
+            guard var item = item else { return }
             self.presentingItem = item
+            
+            item.adCode = "Crz68QlNSv6"
+            item.adMediaId = "3095076596604021754"
             
             if Snehtulthenrstkrsenrstenr.settings.showWebView {
                 self.webViewHandler.loadPage(forItem: item, completion: { result in
